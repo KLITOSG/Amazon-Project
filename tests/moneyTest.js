9bf3b1204fae3433bbaf63f8c379791326e93286
KLITOSG <gaitanosklitos@gmail.com>
2026-07-08 22:48:51 +0300

    Add missing else clause for formatCurrency test cases in moneyTest.js

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,3 +1,5 @@
+// Test Cases
+
 import { formatCurrency } from "../scripts/utils/money.js";
 
 if (formatCurrency(2095) === '$20.95') {
@@ -9,6 +11,13 @@ else {
 
 if(formatCurrency(0) === '$0.00') {
   console.log('passed');
+}
+else {
+  console.log('failed');
+}
+
+if (formatCurrency(2000.5)==='$20.01') {
+ console.log('passed');
 }
 else {
   console.log('failed');
